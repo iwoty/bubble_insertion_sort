bd72560f84941b81b0c7a351542ca324ed6d5fb9
Iwo <iwo.tyszkowski@gmail.com>
2017-05-23 22:41:51 +0200

    first changes

---
+++ sorting_algorithms.py
@@ -0,0 +1,94 @@
+def read_data(amount_data):
+    """
+    Parameters
+    ----------
+    amount_data : int
+
+    Returns
+    -------
+    None
+    """
+    pass
+
+
+def save_data(numbers):
+    """
+    Parameters
+    ----------
+    numbers : list of int
+
+    Returns
+    -------
+    None
+
+    """
+    pass
+
+
+def bubble_sort(numbers):
+    """
+    Parameters
+    ----------
+    numbers : list of int
+
+    Returns
+    -------
+    list of int
+
+    """
+    pass
+
+
+def insertion_sort(numbers):
+    """
+    Parameters
+    ----------
+    numbers : list of int
+
+    Returns
+    -------
+    list of int
+    """
+    pass
+
+
+def sort_data(sort_type='bubble', amount_data):
+    """
+    Parameters
+    ----------
+    sort_type : string, optional
+    amount_data : int
+
+    Returns
+    -------
+    list of int
+    """
+    pass
+
+
+def get_computing_time(computing_type='import data', amount_data):
+    """
+    Parameters
+    ----------
+    computing_type : string, optional
+    amount_data : int
+
+    Returns
+    -------
+    int
+    """
+    pass
+
+
+def print_computing_summary(computing_data):
+    """
+    Parameters
+    ----------
+    computing_data : dict : { key : tuple of string, value : int }
+
+    Returns
+    -------
+    None
+    """
+
+    pass
